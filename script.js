3c3b6ff011110dca657a45d2caeaa1000e37f9b3
BlalAlabd <135108703+BlalAlabd@users.noreply.github.com>
2023-05-31 11:21:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollHome() {
+    let element = document.getElementById('Home');
+    element.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrollProducts() {
+    let element = document.getElementById('products');
+    element.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrollFooter() {
+    let element = document.getElementById('footer');
+    element.scrollIntoView({ behavior: 'smooth' }, true);
+}
